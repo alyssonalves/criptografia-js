010500ba1923c76d8b56d34b9cd1911e23acbf0f
aal <a.alvesdl@gmail.com>
2018-07-24 15:46:54 -0300

    funcionando no home

---
+++ WebContent/WEB-INF/js/aesjs.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+// INIT
+var myString   = "https://www.titanesmedellin.com/";
+var myPassword = "myPassword";
+
+
+// PROCESS
+var encrypted = CryptoJS.AES.encrypt(myString, myPassword);
+var decrypted = CryptoJS.AES.decrypt(encrypted, myPassword);
+document.getElementById("demo0").innerHTML = myString;
+document.getElementById("demo1").innerHTML = encrypted;
+document.getElementById("demo2").innerHTML = decrypted;
+document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
